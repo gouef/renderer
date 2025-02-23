0bf34219ab3efaa539a56367abbd6cef696c9848
Jan Galek <admin@gcore.cz>
2025-02-23 09:54:30 +0100

    [Update] WIP renderer

--- renderer.go
+++ renderer.go
@@ -1,6 +1,8 @@
 package renderer
 
 import (
+	"github.com/gin-contrib/multitemplate"
+	"github.com/gouef/renderer/handlers"
 	"html/template"
 	"net/http"
 	"path/filepath"
@@ -22,4 +24,41 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	if err := t.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func loadTemplates(templatesDir string, templateHandler *handlers.TemplateHandler) multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	funcMap := templateHandler.GetFuncMap()
+
+	layouts, err := filepath.Glob(templatesDir + "/layouts/*.gohtml")
+	if err != nil {
+		panic(err.Error())
+	}
+	rootLayouts, err := filepath.Glob(templatesDir + "/layout.gohtml")
+	if err != nil {
+		panic(err.Error())
+	}
+	layouts = append(layouts, rootLayouts...)
+
+	includes, err := filepath.Glob(templatesDir + "/includes/*.gohtml")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//	includesNamed, err := filepath.Glob(templatesDir + "/**/*.gohtml")
+	/*
+		if err != nil {
+			panic(err.Error())
+		}
+		includes = append(includes, includesNamed...)
+	*/
+	// Generate our templates map from our layouts/ and includes/ directories
+	for _, include := range includes {
+		layoutCopy := make([]string, len(layouts))
+		copy(layoutCopy, layouts)
+		files := append(layoutCopy, includes...)
+		r.AddFromFilesFuncs(filepath.Base(include), funcMap, files...)
+	}
+	return r
 }
